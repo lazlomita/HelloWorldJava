6f0ca5e50d1e5d8964bf8ab8f50fabfb6440e1b0
lazlo.mita <lazlo.mita@gmail.com>
2018-02-05 09:41:55 -0400

    Initial code HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+//Print Hello world in Java programming language
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello world" to the terminal window.
+        System.out.println("Hello world JAVA\n");
+    }
+
+}
